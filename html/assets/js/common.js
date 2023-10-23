d60f269ad28b0fb04327defd8d97d2e2d73a3fbb
(mavin) <ddw6229@naver.com>
2023-10-23 17:21:31 +0900

    <common.js> section 컴포넌트 주석처리

--- html/assets/js/common.js
+++ html/assets/js/common.js
@@ -5,9 +5,9 @@ gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 $(function () {
   $("#header").load("/components/header.html");
   $("#aside").load("/components/sideutil.html");
-  $("#section1").load("/components/views/introduce.html");
-  $("#section2").load("/components/views/skill.html");
-  $("#section3").load("/components/views/portfolio.html");
+  // $("#section1").load("/components/views/introduce.html");
+  // $("#section2").load("/components/views/skill.html");
+  // $("#section3").load("/components/views/portfolio.html");
   $("#footer").load("/components/footer.html");
 });
 
