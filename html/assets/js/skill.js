b85779b4efee18e710f6a97a6256a888c2c55eb7
(mavin) <ddw6229@naver.com>
2023-10-23 15:58:37 +0900

    <skill.js> 애니메이션 주석처리

--- html/assets/js/skill.js
+++ html/assets/js/skill.js
@@ -15,18 +15,12 @@ $(function () {
 });
 
 // gsap scroll animation
-const skillList = document.querySelectorAll("#skill_group li");
-
-skillList.forEach((element) => {
-  gsap.to(element, {
-    scrollTrigger: {
-      trigger: element,
-      start: "0% 75%",
-      end: "80% 80%",
-      toggleClass: "active",
-      scrub: 1,
-    },
-    opacity: 1,
-    marginTop: 15,
-  });
-});
+// const skillList = document.querySelectorAll("#skill_group li");
+// const tl = gsap.timeline();
+// skillList.forEach((element) => {
+//   tl.to(element, {
+//     opacity: 1,
+//     marginTop: 15,
+//     dusration: 1,
+//   },"+=0.5");
+// });
