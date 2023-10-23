294fccaa9bf42ae87c97945e9da03909deabbf39
(mavin) <ddw6229@naver.com>
2023-10-23 15:57:23 +0900

    <common.js> 페이지 동기로드 순서 조정

--- html/assets/js/common.js
+++ html/assets/js/common.js
@@ -1,14 +1,13 @@
 // green shock
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
-ScrollTrigger.config({autoRefreshEvents: "visibilitychange,DOMContentLoaded,load"});
 
 // load component
 $(function () {
+  $("#header").load("/components/header.html");
+  $("#aside").load("/components/sideutil.html");
   $("#section1").load("/components/views/introduce.html");
   $("#section2").load("/components/views/skill.html");
   $("#section3").load("/components/views/portfolio.html");
-  $("#header").load("/components/header.html");
   $("#footer").load("/components/footer.html");
-  $("#aside").load("/components/sideutil.html");
 });
 
